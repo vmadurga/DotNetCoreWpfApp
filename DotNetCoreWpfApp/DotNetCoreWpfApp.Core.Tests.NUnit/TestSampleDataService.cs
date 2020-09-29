e2d9619426272ebcdc79828625c9761ac06a3922
vmadurga <vmadurga@pasiona.com>
2020-09-29 17:46:41 +0200

    Add NUnit tests

---
+++ DotNetCoreWpfApp/DotNetCoreWpfApp.Core.Tests.NUnit/TestSampleDataService.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using DotNetCoreWpfApp.Core.Contracts.Services;
+using DotNetCoreWpfApp.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
+
+namespace DotNetCoreWpfApp.Core.Tests.NUnit
+{
+    public class TestSampleDataService
+    {
+        private IHost _host;
+
+        [SetUp]
+        public void Setup()
+        {
+            var appLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(c => c.SetBasePath(appLocation))
+                .ConfigureServices(ConfigureServices)
+                .Build();
+        }
+
+        private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddTransient<ISampleDataService, SampleDataService>();
+        }
+
+        // TODO WTS: Remove or update this once your app is using real data and not the SampleDataService.
+        // This test serves only as a demonstration of testing functionality in the Core library.
+        [Test]
+        public async Task EnsureSampleDataServiceReturnsContentGridDataAsync()
+        {
+            if (_host.Services.GetService(typeof(ISampleDataService)) is ISampleDataService sampleDataService)
+            {
+                var data = await sampleDataService.GetContentGridDataAsync();
+                Assert.IsTrue(data.Any());
+            }
+            else
+            {
+                Assert.Fail($"Can't resolve {nameof(ISampleDataService)}");
+            }
+        }
+
+        // TODO WTS: Remove or update this once your app is using real data and not the SampleDataService.
+        // This test serves only as a demonstration of testing functionality in the Core library.
+        [Test]
+        public async Task EnsureSampleDataServiceReturnsGridDataAsync()
+        {
+            if (_host.Services.GetService(typeof(ISampleDataService)) is ISampleDataService sampleDataService)
+            {
+                var data = await sampleDataService.GetGridDataAsync();
+                Assert.IsTrue(data.Any());
+            }
+            else
+            {
+                Assert.Fail($"Can't resolve {nameof(ISampleDataService)}");
+            }
+        }
+
+        // TODO WTS: Remove or update this once your app is using real data and not the SampleDataService.
+        // This test serves only as a demonstration of testing functionality in the Core library.
+        [Test]
+        public async Task EnsureSampleDataServiceReturnsMasterDetailDataAsync()
+        {
+            if (_host.Services.GetService(typeof(ISampleDataService)) is ISampleDataService sampleDataService)
+            {
+                var data = await sampleDataService.GetMasterDetailDataAsync();
+                Assert.IsTrue(data.Any());
+            }
+            else
+            {
+                Assert.Fail($"Can't resolve {nameof(ISampleDataService)}");
+            }
+        }
+    }
+}
---
+++ DotNetCoreWpfApp/DotNetCoreWpfApp.Tests.NUnit/TestSettings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using DotNetCoreWpfApp.Contracts.Services;
+using DotNetCoreWpfApp.Models;
+using DotNetCoreWpfApp.ViewModels;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+
+namespace DotNetCoreWpfApp.Tests.NUnit
+{
+    public class TestSettings
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestSettingsViewModel_SetCurrentTheme()
+        {
+            var mockThemeSelectorService = new Mock<IThemeSelectorService>();
+            mockThemeSelectorService.Setup(mock => mock.GetCurrentTheme()).Returns(AppTheme.Light);
+            var mockAppConfig = new Mock<IOptions<AppConfig>>();
+            var mockSystemService = new Mock<ISystemService>();
+            var mockApplicationInfoService = new Mock<IApplicationInfoService>();
+
+            var settingsVm = new SettingsViewModel(mockAppConfig.Object, mockThemeSelectorService.Object, mockSystemService.Object, mockApplicationInfoService.Object);
+            settingsVm.OnNavigatedTo(null);
+
+            Assert.AreEqual(AppTheme.Light, settingsVm.Theme);
+        }
+
+        [Test]
+        public void TestSettingsViewModel_SetCurrentVersion()
+        {
+            var mockThemeSelectorService = new Mock<IThemeSelectorService>();
+            var mockAppConfig = new Mock<IOptions<AppConfig>>();
+            var mockSystemService = new Mock<ISystemService>();
+            var mockApplicationInfoService = new Mock<IApplicationInfoService>();
+
+            var testVersion = new Version(1, 2, 3, 4);
+
+            mockApplicationInfoService.Setup(mock => mock.GetVersion()).Returns(testVersion);
+
+            var settingsVm = new SettingsViewModel(mockAppConfig.Object, mockThemeSelectorService.Object, mockSystemService.Object, mockApplicationInfoService.Object);
+            settingsVm.OnNavigatedTo(null);
+
+            Assert.AreEqual($"DotNetCoreWpfApp - {testVersion}", settingsVm.VersionDescription);
+        }
+    }
+}
